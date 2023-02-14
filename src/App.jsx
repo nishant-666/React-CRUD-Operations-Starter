ac330050ce5d0173424c3fc65c4bac607d4f7a55
Nishant <nishants440@gmail.com>
2023-02-14 20:12:36 +0530

    Starter Code

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+//Import React Router and Axios
+
+function App() {
+  //Create Functions to send requests
+  return <div className="App"></div>;
+}
+
+export default App;
